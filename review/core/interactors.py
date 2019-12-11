bc905bd442ca63b1fea2f50106324fffa152f677
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-12-11 11:48:02 +0330

    Add person review query

--- review/core/interactors.py
+++ review/core/interactors.py
@@ -17,7 +17,7 @@ def save_person_review(reviewee, reviewer, **kwargs):
         return None
 
     # TODO refactor: use get_person_review
-    person_review, created = PersonReview.objects.get_or_create(reviewee=reviewee, reviewer=reviewer)
+    person_review = get_person_review(reviewee, reviewer)
 
     fields = ['sahabiness_rating', 'sahabiness_comment', 'problem_solving_rating',
               'problem_solving_comment', 'execution_rating', 'execution_comment', 'thought_leadership_rating',
@@ -32,6 +32,14 @@ def save_person_review(reviewee, reviewer, **kwargs):
             person_review.__setattr__(field, value)
 
     person_review.save()
+    return person_review
+
+
+def get_person_review(reviewee, reviewer):
+    if not can_review(reviewer, reviewee):
+        return None
+
+    person_review, created = PersonReview.objects.get_or_create(reviewee=reviewee, reviewer=reviewer)
     return person_review
 
 
--- review/core/schema/person_review_query.py
+++ review/core/schema/person_review_query.py
@@ -1,7 +1,11 @@
+import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 
+from accounts.models import User
+from core.interactors import get_person_review
 from core.schema.enums import Evaluation
+from graphql_api.schema.utils import get_node
 from ..models import PersonReview
 
 
@@ -27,3 +31,12 @@ class PersonReviewNode(DjangoObjectType):
     thought_leadership_rating = Evaluation()
     leadership_rating = Evaluation()
     presence_rating = Evaluation()
+
+
+class PersonReviewQuery(graphene.ObjectType):
+    person_review = graphene.Field(PersonReviewNode, reviewee_id=graphene.ID())
+
+    def resolve_person_review(self, info, reviewee_id):
+        reviewee = get_node(reviewee_id, info, User)
+        reviewer = info.context.user
+        return get_person_review(reviewee, reviewer)
