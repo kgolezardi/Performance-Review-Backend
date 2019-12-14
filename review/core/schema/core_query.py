f74f73c6b4b659d84e295a5319dc6ee9e401ab40
Mohsen Davoodi <Davoodi@inside.sahab.ir>
2019-12-14 15:53:26 +0330

    feat: create setting query with static data

--- review/core/schema/core_query.py
+++ review/core/schema/core_query.py
@@ -2,11 +2,12 @@ import graphene
 
 import core.schema.project_query
 import core.schema.person_review_query
-
+import core.schema.setting_query
 
 class Query(
     core.schema.project_query.ProjectQuery,
     core.schema.person_review_query.PersonReviewQuery,
+    core.schema.setting_query.SettingQuery,
     graphene.ObjectType
 ):
     pass
