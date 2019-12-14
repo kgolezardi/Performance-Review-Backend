e7773f33774c07a5e18b1fbf4a12fe7fa0743187
Mohsen Davoodi <Davoodi@inside.sahab.ir>
2019-12-14 17:21:52 +0330

    feat: create project review query

--- review/core/schema/core_query.py
+++ review/core/schema/core_query.py
@@ -3,11 +3,13 @@ import graphene
 import core.schema.project_query
 import core.schema.person_review_query
 import core.schema.setting_query
+import core.schema.project_review_query
 
 class Query(
     core.schema.project_query.ProjectQuery,
     core.schema.person_review_query.PersonReviewQuery,
     core.schema.setting_query.SettingQuery,
+    core.schema.project_review_query.ProjectReviewQuery,
     graphene.ObjectType
 ):
     pass
