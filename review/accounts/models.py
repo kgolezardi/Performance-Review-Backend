9ad6ccb00760499792df8d9be5e2842aa7c1505c
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-12-16 16:35:59 +0330

    Sort models by name and time created

--- review/accounts/models.py
+++ review/accounts/models.py
@@ -6,5 +6,8 @@ class User(AbstractUser):
     manager = models.ForeignKey("User", related_name='team_members', on_delete=models.SET_NULL, null=True, blank=True)
     employee_id = models.IntegerField(unique=True, null=True)
 
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
     def __str__(self):
         return self.username
