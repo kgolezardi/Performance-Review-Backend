922bcab65041de0b759aadbff0d46984b37dffa5
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-12-30 15:29:36 +0330

    improve docker-compose

--- review/review/settings.py
+++ review/review/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 STATIC_ROOT = root('django-statics')
 
 # Local settings
