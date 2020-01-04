ecc21eb52f6301abd7ca51f5e884e34795fa51bc
Kiarash Golezardi <golezardi@ce.sharif.edu>
2020-01-04 22:05:11 +0330

    Add import users by CSV in Django admin

--- review/accounts/admin.py
+++ review/accounts/admin.py
@@ -1,7 +1,14 @@
-import django.contrib.auth.admin
-from django.contrib import admin
+import csv
+from io import StringIO
 
-from .forms import UserCreationForm, UserChangeForm
+import django.contrib.auth.admin
+from django.contrib import admin, messages
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
+from django.urls import path
+
+from accounts.interactors import add_user
+from .forms import UserCreationForm, UserChangeForm, CsvRowValidationForm, CsvImportForm
 from .models import User
 
 
@@ -15,3 +22,39 @@ class UserAdmin(django.contrib.auth.admin.UserAdmin):
         ('Reiew data', {'fields': ('has_started',)}),
     )
     list_filter = django.contrib.auth.admin.UserAdmin.list_filter + ('has_started',)
+    change_list_template = 'accounts/change_list_with_import.html'
+
+    def get_urls(self):
+        my_urls = [path('import-users', self.admin_site.admin_view(self.add_users))]
+        return my_urls + super().get_urls()
+
+    def add_users(self, request):
+        if request.method == "POST":
+            csv_content = StringIO(request.FILES["csv_file"].read().decode('utf-8'))
+            reader = csv.DictReader(csv_content, delimiter=',')
+            successful, unsuccessful = 0, 0
+            for row in reader:
+                success = False
+                print(row)
+                if CsvRowValidationForm(row).is_valid():
+                    username = row['email'].split('@')[0]
+                    success = add_user(username=username, **row)
+                if success:
+                    successful += 1
+                else:
+                    unsuccessful += 1
+
+            level = messages.SUCCESS if unsuccessful == 0 else messages.WARNING
+            self.message_user(request, "Added %d users successfully, and %d row(s) had error."
+                              % (successful, unsuccessful), level)
+            return redirect('.')
+
+        form = CsvImportForm()
+        context = {
+            'title': 'Import users',
+            'form': form,
+            'opts': self.model._meta,
+            **self.admin_site.each_context(request)
+        }
+        return TemplateResponse(request, 'accounts/import_users.html', context)
+
