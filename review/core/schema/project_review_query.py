2819fc352423b6c03f5b9528a57f087c60b5b038
Kiarash Golezardi <golezardi@ce.sharif.edu>
2020-02-17 15:22:04 +0330

    Filter project reviews by user

--- review/core/schema/project_review_query.py
+++ review/core/schema/project_review_query.py
@@ -2,11 +2,13 @@ import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 
+from accounts.models import User
 from accounts.schema.user_query import UserNode
 from core.enums import Phase
 from core.interactors.project_review import get_all_project_reviews, get_project_review, get_users_to_review
 from core.interactors.settings import is_at_phase
 from core.schema.enums import Evaluation
+from graphql_api.schema.utils import get_node
 from ..models import ProjectReview
 
 
@@ -35,12 +37,17 @@ class ProjectReviewNode(DjangoObjectType):
 
 class ProjectReviewQuery(graphene.ObjectType):
     project_review = relay.Node.Field(ProjectReviewNode)
-    project_reviews = graphene.List(graphene.NonNull(ProjectReviewNode), required=True)
+    project_reviews = graphene.List(graphene.NonNull(ProjectReviewNode), reviewee_id=graphene.ID(), required=True)
 
     users_to_review = graphene.List(graphene.NonNull(UserNode), required=True)
 
-    def resolve_project_reviews(self, info):
-        return get_all_project_reviews(info.context.user)
+    def resolve_project_reviews(self, info, reviewee_id=None):
+        user = info.context.user
+        project_reviews = get_all_project_reviews(user=user)
+        if reviewee_id is not None:
+            reviewee = get_node(reviewee_id, info, User)
+            project_reviews = project_reviews.filter(reviewee=reviewee)
+        return project_reviews
 
     def resolve_users_to_review(self, info):
         return get_users_to_review(info.context.user)
