ec73416a30fc56eb82f539e51e8eefc937bd2285
Kiarash Golezardi <kia.masster@yahoo.com>
2021-11-20 10:21:42 +0330

    Update CI to push docker images with explicit tags (#99)

--- scripts/docker_push_dev.sh
+++ scripts/docker_push_dev.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 bash scripts/docker_build_dev.sh
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker push kgolezardi/performance-review-api
-docker push kgolezardi/performance-review-statics
+docker push kgolezardi/performance-review-api:dev
+docker push kgolezardi/performance-review-api:dev-$TRAVIS_COMMIT
+docker push kgolezardi/performance-review-api:dev-$TRAVIS_BUILD_NUMBER
+docker push kgolezardi/performance-review-statics:dev
+docker push kgolezardi/performance-review-statics:dev-$TRAVIS_COMMIT
+docker push kgolezardi/performance-review-statics:dev-$TRAVIS_BUILD_NUMBER
