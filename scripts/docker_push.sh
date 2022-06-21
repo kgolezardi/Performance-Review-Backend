99a8816037c1bf0f3211a7cb5ff2ea30ab4a4b0f
Kiarash Golezardi <golezardi@ce.sharif.edu>
2022-06-21 18:59:53 +0430

    Use Github actions instead of Travis

--- scripts/docker_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bash scripts/docker_build.sh
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker push kgolezardi/performance-review-api
-docker push kgolezardi/performance-review-statics
--- scripts/docker_push_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-bash scripts/docker_build_dev.sh
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker push kgolezardi/performance-review-api:dev
-docker push kgolezardi/performance-review-api:dev-$TRAVIS_COMMIT
-docker push kgolezardi/performance-review-api:dev-$TRAVIS_BUILD_NUMBER
-docker push kgolezardi/performance-review-statics:dev
-docker push kgolezardi/performance-review-statics:dev-$TRAVIS_COMMIT
-docker push kgolezardi/performance-review-statics:dev-$TRAVIS_BUILD_NUMBER
